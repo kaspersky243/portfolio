3d53032190fe817ede71c3cecfe857b37e776956
kaspersky243 <hermann.okou@epitech.eu>
2025-11-19 11:32:24 +0000

    Portfolio

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -15,7 +15,7 @@ export default function AboutSection() {
           transition={{ duration: 1 }}
         >
           <img
-            src="/imge.png" // Remplacer par ta photo ou illustration
+            src="/OKOU HERMANN AUGUSTIN.png" // Remplacer par ta photo ou illustration
             alt="Okou Hermann"
             className="rounded-xl shadow-lg w-full object-cover"
           />
